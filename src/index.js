180857748b89d58381b9ebe6ec5e21fdff8af768
Yves Radenac <yradenac@sogec-marketing.fr>
2019-06-25 11:30:30 +0200

    Add ThemeProvider makes the code fail

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import { ThemeProvider } from '@material-ui/styles'
+import { createMuiTheme } from '@material-ui/core'
 
 class Boilerplate extends Component {
   render() {
+    const theme = createMuiTheme({
+
+    })
+    
     return (
       <React.Fragment>
-        <div className='title'>Hello world</div>
+        <ThemeProvider theme={theme}>
+          <div className='title'>Hello world</div>
+        </ThemeProvider>
       </React.Fragment>
     )
   }
